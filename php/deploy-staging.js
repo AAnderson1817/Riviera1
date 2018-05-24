cfe89cade712b58908ac6e14de1802d8f74aaeec
Garrett Estrin <garrett.estrin@gmail.com>
2018-05-23 17:37:01 -0700

    set up staging env

---
+++ php/deploy-staging.js
@@ -0,0 +1,32 @@
+var FtpDeploy = require('ftp-deploy-log'),
+ftpDeploy = new FtpDeploy(),
+fs = require('fs'),
+dotenv = require('dotenv').config(),
+excluded = fs.readFileSync('../.gitignore', 'utf8');
+excluded = excluded.split(/[\r\n]+/);
+ 
+var config = {
+    username: process.env.STAGEFTPUSER,
+    password: process.env.STAGEFTPPASSWORD, // optional, prompted if none given 
+    host: process.env.STAGEFTPHOST,
+    port: 21,
+    localRoot: __dirname,
+    remoteRoot: "/",
+    include: ['build/version.txt'],
+    exclude: excluded,
+    useLog: true,
+    staging: true
+}
+    
+ftpDeploy.deploy(config, function(err) {
+    if (err) console.log(err)
+    else console.log('finished');
+});
+
+ftpDeploy.on('uploading', function(data) {
+    data.totalFileCount;       // total file count being transferred 
+    data.transferredFileCount; // number of files transferred 
+    data.percentComplete;      // percent as a number 1 - 100 
+    data.filename;             // partial path with filename being uploaded 
+    console.log(data);
+});
