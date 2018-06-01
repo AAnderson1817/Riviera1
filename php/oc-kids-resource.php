318325ccdcb84dbf58b6f03c55afb2680058a81f
Garrett Estrin <garrett.estrin@gmail.com>
2018-05-31 21:13:54 -0700

    moved publications into iframe in site and set up banner ads

--- php/oc-kids-resource.php
+++ php/oc-kids-resource.php
@@ -3,7 +3,7 @@
   <div class="agileits-banner jarallax">
     <?php include 'navigation.php' ?>
       <div class="container">
-        <img src="/images/ads/flight.gif" alt="" class="banner-ad">
+        <img src="/images/ads/flight.gif" alt="" class="banner-image">
         <iframe class="iframe" id="ocKidsiframe" src="http://www.webpublished.com/gallery/view.asp?seq=310438&path=170829035804" frameborder="0"></iframe>
       </div>
   </div>
