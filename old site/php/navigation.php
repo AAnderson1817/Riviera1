6bf4d7c1309e4dbc0b31b2cd04e4ae1d7f982f30
Garrett Estrin <garrett.estrin@gmail.com>
2018-07-23 18:58:13 -0700

    updated file structure

---
+++ old site/php/navigation.php
@@ -0,0 +1,84 @@
+<!-- navigation -->
+<div class="top-nav w3-agiletop">
+	<div class="container">
+		<div class="navbar-header w3llogo">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<h1><a href="/">Riviera Publications<span>Laguna Beach, CA</span> </a></h1>
+		</div>
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<div class="w3menu navbar-right">
+				<ul class="nav navbar">
+					<li><a href="./" class="<?php if($page == 'index.php'){echo 'active';} ?>">Home</a></li>
+					<li><a href="./about.php" class="<?php if($page == 'about.php'){echo 'active';} ?>">About</a></li>
+					<li><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span data-letters="Pages">Publications</span><span class="caret"></span></a>
+						<ul class="dropdown-menu">
+
+							<li id="ddown-subhead">Check Out Live Previews Below</li>
+							<li>
+                <a href="http://www.webpublished.com/gallery/view.asp?seq=299041&path=161216154504">Laguna Neighborhood Directory</a>
+              </li>
+              <ul>
+                <a class="testimonial" href="#" data-toggle="modal" data-target="#LagunaNeighborhoodModal">Testimonials</a>
+              </ul>
+
+							<li>
+                <a href="http://fbs.advantageinc.com/rivierapublishing/lagunawoods2018/">Laguna Resource Map</a>
+              </li>
+
+              <ul>
+                <a class="testimonial" href="#" data-toggle="modal" data-target="#LagunaResourceModal">Testimonials</a>
+              </ul>
+
+							<li>
+                <a href="/oc-kids-resource.php">OC Kid's Resource</a>
+              </li>
+
+              <ul>
+                <a class="testimonial" href="#" data-toggle="modal" data-target="#OCKidModal">Testimonials</a>
+              </ul>
+
+							<li>
+                <a href="http://www.webpublished.com/gallery/view.asp?seq=316997&path=180216043948">OC Senior's Directory</a>
+              </li>
+
+              <ul>
+                <a class="testimonial" href="#" data-toggle="modal" data-target="#OCSeniorModal">Testimonials</a>
+              </ul>
+							<li>
+                <a href="/laguna-festival-guide.php">Laguna Festival Guide</a>
+              </li>
+
+              <ul>
+                <a class="testimonial" href="#" data-toggle="modal" data-target="#OCLagunaFestival">Testimonials</a>
+              </ul>
+
+							<!-- <li>
+                <a href="http://www.webpublished.com/gallery/view.asp?seq=286034&path=151121051453">Inland Empire Senior's Directory</a>
+              </li>
+
+              <ul>
+                <a class="testimonial" href="#" data-toggle="modal" data-target="#IESeniorModal">Testimonials</a>
+              </ul>
+
+							<li>
+                <a href="http://www.webpublished.com/gallery/view.asp?seq=294972&path=160802064930">San Diego Kid's Resource</a>
+              </li>
+              <ul>
+                <a class="testimonial" href="#" data-toggle="modal" data-target="#SDKidModal">Testimonials</a>
+              </ul> -->
+						</ul>
+					</li>
+					<li><a href="#" data-toggle="modal" data-target="#ContactUsModal">Contact Us</a></li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+		</div>
+	</div>
+</div>
+<!-- //navigation -->
